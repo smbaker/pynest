f20cfe925a3a9dc72aceda3b63eb2904dcb7998e
smbaker <smbaker@gmail.com>
2012-05-11 05:41:49 -0700

    add python setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pynest',
+      version='1.0',
+      description='Python API for Nest Thermostat',
+      author='Scott Baker',
+      author_email='smbaker@gmail.com',
+      url='http://www.smbaker.com/',
+      scripts=['nest.py'],
+     )
