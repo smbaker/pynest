d42b7cca6cf6052273d0ec322c35c8a6e5de4d8d
Eugene Efremov <eaefremov@gmail.com>
2013-03-09 23:30:07 -0800

    fix import

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 readme = open('README.txt').read()
 setup(name='pyenest',
       version='0.2',
