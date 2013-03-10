48d946fb51f0039b63b598b4958a68e3150a5042
Eugene Efremov <eaefremov@gmail.com>
2013-03-09 22:02:15 -0800

    further refactor to use this as a library

--- nest.py
+++ nest.py
@@ -86,9 +86,10 @@ class Nest:
         #print "res[device][serial].keys", res["device"][self.serial].keys()
         #print "res[shared][serial].keys", res["shared"][self.serial].keys()
 
-    def show_status(self):
-        shared = self.status["shared"][self.serial]
-        device = self.status["device"][self.serial]
+    def show_status(self, serial=None):
+        serial = serial or self.serial
+        shared = self.status["shared"][serial]
+        device = self.status["device"][serial]
 
         allvars = shared
         allvars.update(device)
@@ -96,15 +97,17 @@ class Nest:
         for k in sorted(allvars.keys()):
              print k + "."*(32-len(k)) + ":", allvars[k]
 
-    def show_curtemp(self):
-        temp = self.status["shared"][self.serial]["current_temperature"]
+    def show_curtemp(self, serial=None):
+        serial = serial or self.serial
+        temp = self.status["shared"][serial]["current_temperature"]
 
         print "%0.1f" % temp
 
-    def set_temperature(self, temp):
+    def set_temperature(self, temp, serial=None):
+        serial = serial or self.serial
 
         data = '{"target_change_pending":true,"target_temperature":' + '%0.1f' % temp + '}'
-        req = urllib2.Request(self.transport_url + "/v2/put/shared." + self.serial,
+        req = urllib2.Request(self.transport_url + "/v2/put/shared." + serial,
                               data,
                               {"user-agent":"Nest/1.1.0.10 CFNetwork/548.0.4",
                                "Authorization":"Basic " + self.access_token,
@@ -114,9 +117,10 @@ class Nest:
 
         print res
 
-    def set_fan(self, state):
+    def set_fan(self, state, serial=None):
+        serial = serial or self.serial
         data = '{"fan_mode":"' + str(state) + '"}'
-        req = urllib2.Request(self.transport_url + "/v2/put/device." + self.serial,
+        req = urllib2.Request(self.transport_url + "/v2/put/device." + serial,
                               data,
                               {"user-agent":"Nest/1.1.0.10 CFNetwork/548.0.4",
                                "Authorization":"Basic " + self.access_token,
@@ -126,10 +130,11 @@ class Nest:
 
         print res
     
-    def toggle_away(self):
-        was_away = self.status['structure'][self.structure_id]['away']
+    def toggle_away(self, structure_id=None):
+        structure_id = structure_id or self.structure_id
+        was_away = self.status['structure'][structure_id]['away']
         data = '{"away":%s}' % ('false' if was_away else 'true')
-        req = urllib2.Request(self.transport_url + "/v1/put/structure." + self.structure_id,
+        req = urllib2.Request(self.transport_url + "/v1/put/structure." + structure_id,
                               data,
                               {"user-agent":"Nest/1.1.0.10 CFNetwork/548.0.4",
                                "Authorization":"Basic " + self.access_token,
@@ -222,8 +227,3 @@ def main():
 
 if __name__=="__main__":
    main()
-
-
-
-
-
